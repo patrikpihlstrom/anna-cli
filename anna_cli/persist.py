7fc393844ba4f5b73f90351d6e64166ce4b79dc3
Patrik Pihlstrom <patrik.pihlstrom@gmail.com>
2019-02-21 02:01:44 +0100

    global config path

--- anna_cli/persist.py
+++ anna_cli/persist.py
@@ -1,10 +1,14 @@
 import os, json
 
 
+config = os.path.dirname(__file__) + '/config.json'
+
+
 def get(key):
-	if not os.path.isfile('config.json'):
+	global config
+	if not os.path.isfile(config):
 		return ''
-	with open('config.json', 'r') as f:
+	with open(config, 'r') as f:
 		config = json.load(f)
 		if key in config:
 			return config[key]
@@ -12,15 +16,16 @@ def get(key):
 
 
 def set(key, val):
+	global config
 	if not isinstance(val, str):
 		return False
 
-	if not os.path.isfile('config.json'):
-		with open('config.json', 'a') as f:
+	if not os.path.isfile(config):
+		with open(config, 'a') as f:
 			f.write('{}')
 			f.close()
 
-	with open('config.json', 'r+') as f:
+	with open(config, 'r+') as f:
 		data = json.load(f)
 		data[key] = val
 		f.seek(0)  # <--- should reset file position to the beginning.
