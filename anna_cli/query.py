2b3cba0d46d548670ee04f7e5c6569f457e69132
Patrik Pihlström <patrik.pihlstrom@caupo.se>
2019-02-22 17:21:19 +0100

    fix job request construction

--- anna_cli/query.py
+++ anna_cli/query.py
@@ -1,5 +1,4 @@
 import json
-from pprint import pprint
 
 
 def is_json(string):
@@ -14,18 +13,10 @@ def is_json(string):
 
 def job(drivers, sites):
 	result = {}
-	if is_json(drivers):
-		result['drivers'] = json.loads(drivers)
-	elif drivers is not None and len(drivers) > 0:
-		result['drivers'] = str(drivers)
-	if is_json(sites):
-		result['sites'] = json.loads(sites)
-	elif sites is not None and len(sites) > 0:
-		result['sites'] = str(sites)
-	if not isinstance(result['drivers'], list):
-		result['drivers'] = [result['drivers']]
-	if not isinstance(result['sites'], list):
-		result['sites'] = [result['sites']]
+	if drivers is not None and len(drivers) > 0:
+		result['drivers'] = str(drivers).split(',')
+	if sites is not None and len(sites) > 0:
+		result['sites'] = str(sites).split(',')
 	return result
 
 
@@ -43,6 +34,5 @@ def filter(id, container, driver, site, status, tag):
 		result['status'] = str(status).split(',')
 	if tag is not None and len(tag) > 0:
 		result['tag'] = str(tag).split(',')
-	pprint(result)
 	return result
 
